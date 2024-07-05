70632a62f4c88a5aac23d8b441686d53b89aa051
dinhphucaz52 <dinhphucaz52@gmail.com>
2024-07-05 17:21:56 +0700

    5/7/2024

--- main.cpp
+++ main.cpp
@@ -2,11 +2,21 @@
 #include <queue>
 #include <fstream>
 #include <algorithm>
-#include <stack>
+
 
 using namespace std;
 
 const int MAXM = 2e3;
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> list)
+{
+    cerr << "{";
+    for (auto x : list)
+        cerr << x << ", ";
+    cerr << "}";
+    return os;
+}
 
 struct Edge
 {
@@ -33,11 +43,23 @@ class Graph
 {
 private:
     int n;
-    int **capacity;
-    int *par;
+    int **curCapacity; // curCapacity[u][v]
+    int **tmp; 
+    int *par; //par[u]
     vector<AugmentingPath> augmentingPathList;
     vector<Edge> edgeList;
 
+    void traceBack(int minFlow, int s, int t)
+    {
+        AugmentingPath tmp;
+        tmp.flow = minFlow;
+        for (int v = t; v != s; v = par[v])
+            tmp.pushVertex(v);
+        tmp.pushVertex(s);
+        tmp.finish();
+        this->augmentingPathList.push_back(tmp);
+    }
+    
     bool bfs(int s, int t)
     {
         vector<bool> vst(n, 0);
@@ -50,7 +72,7 @@ private:
             q.pop();
             for (int v = 0; v < n; v++)
             {
-                if (!vst[v] && this->capacity[u][v] > 0)
+                if (!vst[v] && this->curCapacity[u][v] > 0)
                 {
                     if (v == t)
                         return this->par[v] = u, true;
@@ -63,27 +85,22 @@ private:
         return false;
     }
 
-    void traceBack(int minFlow, int s, int t)
-    {
-        AugmentingPath tmp;
-        tmp.flow = minFlow;
-        for (int v = t; v != s; v = par[v])
-            tmp.pushVertex(v);
-        tmp.pushVertex(s);
-        tmp.finish();
-        this->augmentingPathList.push_back(tmp);
-    }
-
 public:
     Graph(int size)
     {
         this->n = size;
-        this->capacity = new int *[size];
+        this->curCapacity = new int *[size];
+        this->tmp = new int *[size];
         for (int i = 0; i < size; i++)
-            this->capacity[i] = new int[size];
+        {
+            this->curCapacity[i] = new int[size];
+            this->tmp[i] = new int[size];
+        }
         for (int u = 0; u < size; u++)
             for (int v = 0; v < size; v++)
-                this->capacity[u][v] = 0;
+            {
+                this->curCapacity[u][v] = this->tmp[u][v] = 0;
+            }
         this->par = new int[size];
         this->augmentingPathList = vector<AugmentingPath>();
         this->edgeList = vector<Edge>();
@@ -92,7 +109,9 @@ public:
     Graph(int size, vector<Edge> edgeList) : Graph(size)
     {
         for (auto [u, v, w] : edgeList)
-            capacity[u][v] = w;
+        {
+            curCapacity[u][v] = tmp[u][v] = w;
+        }
         this->edgeList = edgeList;
     }
 
@@ -105,15 +124,16 @@ public:
             for (int v = t; v != s; v = par[v])
             {
                 int u = par[v];
-                minFlow = min(minFlow, capacity[u][v]);
+                minFlow = min(minFlow, curCapacity[u][v]);
             }
+
             this->traceBack(minFlow, s, t); // Chi de phuc vu preview do thi
 
             for (int v = t; v != s; v = par[v])
             {
                 int u = par[v];
-                capacity[u][v] -= minFlow;
-                capacity[v][u] += minFlow;
+                curCapacity[u][v] -= minFlow;
+                curCapacity[v][u] += minFlow;
             }
             res += minFlow;
         }
@@ -132,8 +152,8 @@ public:
             AugmentingPath path = this->augmentingPathList[i];
             if (i + 1 == augmentingPathCnt)
             {
-                cerr << "Res: " << cur << " + " << " " << this->augmentingPathList[i].flow;
-                cerr << endl;
+                // cerr << "Res: " << cur << " + " << " " << this->augmentingPathList[i].flow;
+                // cerr << endl;
             }
             cur += this->augmentingPathList[i].flow;
             ofs << path.flow << " : ";
@@ -185,9 +205,49 @@ public:
         ofs << "}\n";
         ofs.close();
     }
+    void printMinCut(int s, int t)
+    {
+        this->fordFulkerson(s, t);
+        vector<bool> vst(this->n);
+        vector<int> setS = {s};
+        queue<int> q;
+        vst[s] = true;
+        q.push(s);
+
+        while (!q.empty())
+        {
+            int u = q.front();
+            q.pop();
+            for (int v = 0; v < this->n; v++)
+            {
+                if (!vst[v] && this->curCapacity[u][v])
+                {
+                    q.push(v);
+                    vst[v] = true;
+                    setS.push_back(v);
+                }
+            }
+        }
+        vector<int> setT = {};
+        for (int u = 0; u < this->n; u++)
+            if (!vst[u])
+                setT.push_back(u);
+                
+        cerr << "setS : " << setS << "\n";
+        cerr << "setT : " << setT << "\n";
+        
+        for (int u : setS)
+        {
+            for (int v : setT)
+            {
+                if (tmp[u][v])
+                    cerr << u << " " << v << " " << this->curCapacity[u][v] + this->curCapacity[v][u] << "\n";
+            }
+        }
+    }
     ~Graph()
     {
-        free(this->capacity);
+        free(this->curCapacity);
         free(this->par);
     }
 };
@@ -214,9 +274,13 @@ int main()
 
     Graph graph = Graph(n, edgeList);
 
-    cerr << graph.fordFulkerson(0, n - 1) << endl;
+    cerr << "Max flow:" << graph.fordFulkerson(0, n - 1) << endl;
+
+    cerr << "Min cut:\n";
+    graph.printMinCut(0, n - 1);
 
     int step = 0;
+
     for (step = -1; step < 5; step++)
     {
         graph.printForDotFile("dot.dot", step == -1 ? 0 : step);
@@ -224,20 +288,11 @@ int main()
     }
     return 0;
 }
+
 /*
 8 : 0 1 3 5
 2 : 0 1 4 5
 2 : 0 2 3 5
 6 : 0 2 3 4 5
 1 : 0 2 3 1 4 5
-*/
-/*
-Ban đầu truyền từ 0 -> 1 -> 3 -> 5 : 8
-Ta có cạnh reverse 3 -> 1 : 1
-                    8
-Ban đầu truyền từ 0 -> 1 -> 3 -> 5 : 7
-Sẽ được truyền từ 1 -> 4 -> 5 : 1
-
-Còn 1 năng lượng từ 0 -> 2 -> 3 -> 5
-
 */
